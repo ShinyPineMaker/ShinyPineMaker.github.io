c65eb0ceabf3de66b2ba504128390418b736591e
ShinyPineMaker <guizin.lps@gmail.com>
2024-09-21 16:12:15 -0300

    added aula_4

---
+++ Aulas/Scripts/aula_4.js
@@ -0,0 +1,17 @@
+const aluno = () => {
+    nome: String;
+    email:String;
+    dataNasc: Array;
+    Telefone: Array;
+    Curso: String;
+    Matricula: String;
+}
+
+const professor = () => {
+    nome: String;
+    email:String;
+    dataNasc: Array;
+    Telefone: Array;
+    Curso: String;
+    Matricula: String;
+}
