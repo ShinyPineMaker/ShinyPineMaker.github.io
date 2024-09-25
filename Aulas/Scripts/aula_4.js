793dfc855402be72160ad18b1bc8f888fe419c1a
ShinyPineMaker <guizin.lps@gmail.com>
2024-09-25 12:14:03 -0300

    Add aula4.5 html and Scripts add local storage in aula 4

--- Aulas/Scripts/aula_4.js
+++ Aulas/Scripts/aula_4.js
@@ -35,22 +35,228 @@ const area_curso = document.querySelector("#area-curso");
 const matricula = document.querySelector("#matricula");
 const lattes = document.querySelector("#lattes");
 
+//campo de validacao coisas
+const v_nome = document.querySelector("#validacao-nome");
+const v_email = document.querySelector("#validacao-email");
+const v_data_n = document.querySelector("#validacao-data-nasc");
+const v_tel_f = document.querySelector("#validacao-fixo");
+const v_tel_c = document.querySelector("#validacao-celular");
+const v_area_curso = document.querySelector("#validacao-area-curso");
+const v_matricula = document.querySelector("#validacao-matricula");
+const v_lattes = document.querySelector("#validacao-lattes");
+
+
 //Elementos HTML coisas
+const form = document.querySelector("#form");
 const radio_professor = document.querySelector("#professor");
 const radio_aluno = document.querySelector("#aluno");
 const area_curso_label = document.querySelector("#area-curso-label");
 const row_lattes = document.querySelector("#row-lattes");
 
-// myFunction()
-const ViewProfessor = () => { 
+let registro = "Professor";
+let conforme = false;
+
+// views
+const viewProfessor = () => { 
     area_curso_label.innerHTML = "Área: ";
     row_lattes.classList.remove("hidden");
+    registro = "Professor";
 }
 
-const ViewAluno = () => {
+const viewAluno = () => {
     area_curso_label.innerHTML = "Curso: ";
     row_lattes.classList.add("hidden");
+    registro = "Aluno";
 }
 
-radio_professor.addEventListener('click', ViewProfessor);
-radio_aluno.addEventListener('click', ViewAluno);
+radio_professor.addEventListener('click', viewProfessor);
+radio_aluno.addEventListener('click', viewAluno);
+
+
+// validações
+/* 
+    Nome: xxx xxx
+    Email: xxx@xxx.xxx
+    DataNasc: dd/mm/aaaa
+    Telefone-fixo: (xx)xxxx-xxxx > preencher automático traço e parentesis
+    Telefone-celular: (xx)xxxx-xxxx > preencher automático traço e parentesis
+    curso/area: obrigatório
+    
+    matrícula   > professor : 5 digitos
+                > aluno : 10 digitos
+*/
+
+const validarNome = () => {
+    let str = nome.value;
+    let regex = /[!@#$%^&*(),.?":{}|<>]/;
+    if (!regex.test(str)){
+        v_nome.innerHTML = "";
+        return true;
+    }
+    else{
+        v_nome.innerHTML = "Insira nome e sobrenome válidos";
+        nome.value = "";
+        return false;
+    }
+    
+}
+nome.addEventListener('blur', conforme = validarNome);
+
+const validarEmail = () => {
+    let str=email.value;
+    if (str.includes("@") && str.includes(".")){
+        v_email.innerHTML = "";
+        return true;
+    }
+    else{
+        v_email.innerHTML = "Insira um email válido";
+        email.value = "";
+        return false;
+    }
+}
+email.addEventListener('blur', validarEmail);
+
+const verificarTelefone = (e) => {
+    let value = e.target.value.replace(/\D/g, '');
+    if (value.length > 10) {
+        value = value.slice(0, 10);
+    }
+    const formattedValue = formatarTelefone(value);
+    e.target.value = formattedValue;
+}
+
+const formatarTelefone = (value) => {
+    if (value.length < 3) return value;
+    const ddd = value.slice(0, 2);
+    const prefix = value.slice(2, 6);
+    const suffix = value.slice(6, 10);
+    if (suffix) {
+        return `(${ddd})${prefix}-${suffix}`;
+    } else if (prefix) {
+        return `(${ddd})${prefix}`;
+    } else {
+        return `(${ddd})`;
+    }
+}
+
+const validarTelefone = (e) => {
+    if (e.target == tel_f){
+        target = v_tel_f;
+    }
+    else if (e.target == tel_c){
+        target = v_tel_c;
+    }
+
+    if (e.target.value.length <= 12){
+        target.innerHTML = "Insira um telefone válido";
+    }
+    else {
+        target.innerHTML = "";
+    }
+}
+
+tel_f.addEventListener('input', verificarTelefone);
+tel_c.addEventListener('input', verificarTelefone);
+tel_f.addEventListener('blur', validarTelefone);
+tel_c.addEventListener('blur', validarTelefone);
+
+
+
+const limitarMatricula = () => {
+    if (registro == "Aluno") {
+        if (matricula.value.length > 10){
+            matricula.value = matricula.value.slice(0,10);
+        }
+    }
+
+    else if (registro == "Professor"){
+        if (matricula.value.length > 5){
+            matricula.value = matricula.value.slice(0,5);
+        }
+    }
+}
+
+const validarMatricula = () => {
+    if (registro == "Aluno") {
+        if (matricula.value.length < 10){
+            v_matricula.innerHTML = "Insira uma matrícula válida"
+            return false;
+        }
+        else{
+            v_matricula.innerHTML = "";
+            return true;
+        }
+    }
+    else if (registro == "Professor"){
+        if (matricula.value.length < 5){
+            v_matricula.innerHTML = "Insira uma matrícula válida"
+            return false;
+        }
+        else{
+            v_matricula.innerHTML = "";
+            return true;
+        }
+    }
+}
+
+matricula.addEventListener('input', limitarMatricula);
+matricula.addEventListener('blur', validarMatricula)
+
+validarAreaCurso = () => {
+    if (area_curso.value.length == 0){
+        v_area_curso.innerHTML = "Preencha esse campo";
+        return false;
+    }
+    v_area_curso.innerHTML = "";
+    return true;
+}
+
+area_curso.addEventListener('blur', validarAreaCurso);
+
+validarLattes = () => {
+    if (lattes.value.length == 0){
+        v_lattes.innerHTML = "Preencha esse campo";
+        return false;
+    }
+    v_lattes.innerHTML = "";
+    return true;
+}
+
+lattes.addEventListener('blur', validarLattes);
+
+let contd = 0
+salvarDados = (event) => {
+    event.preventDefault();
+
+    if (registro == "Professor"){
+        p = new Professor(
+            nome.value, 
+            email.value, 
+            data_n.value, 
+            tel_f.value, 
+            tel_c.value, 
+            matricula.value, 
+            area_curso.value, 
+            lattes.value
+        );
+        localStorage.setItem("obj"+contd, JSON.stringify(p));
+    }
+
+    if (registro == "Aluno"){
+        a = new Aluno(
+            nome.value,
+            email.value,
+            data_n.value,
+            tel_f.value,
+            tel_c.value,
+            matricula.value,
+            area_curso.value
+        );
+        localStorage.setItem("obj"+contd, JSON.stringify(a));
+    }
+    contd += 1;
+    localStorage.setItem("contd", contd);
+    form.reset();
+}
+
+enviar.addEventListener('click', salvarDados);
